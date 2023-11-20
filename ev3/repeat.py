5f9365c03aa3b2c611fb2f9c87a1c4d40930f945
Adam Thomas <thomasadam83@hotmail.com>
2023-11-20 16:07:05 -0500

    Changed repeat.py

--- ev3/repeat.py
+++ ev3/repeat.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import ColorSensor
+from pybricks.parameters import Port, Button, Color
+from pybricks.tools import wait
+
+# Initialize the EV3 Brick
+ev3 = EV3Brick()
+
 def execute():
   
   print('REPEAT UPDATE 3')
+
+  ev3.speaker.say("Hello")

b89c41aa28b63f1273d1e635a8e76be62e55ce62
Adam Thomas <thomasadam83@hotmail.com>
2023-11-20 16:08:06 -0500

    Changed repeat.py

--- ev3/main.py
+++ ev3/main.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-
 from sys import exit
 
-import os
-import time
+import os, time
 
 import urequests as requests
 import ujson as json
